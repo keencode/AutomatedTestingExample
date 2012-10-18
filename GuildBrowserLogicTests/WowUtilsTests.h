f9534ae5482975b6926661cca8c088a0d9056103
Yee Peng Chia <peng@cocoastarapps.com>
2012-10-18 11:38:18 -0400

    Added tests.

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Yee Peng Chia on 10/18/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
